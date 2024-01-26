b2578e65e7b9e2c792b89ea452f4be54dcf6ca5b
2CHEVSKII <prsroman3@gmail.com>
2024-01-26 07:51:56 +0300

    added Option<->Result conversion extensions & tests

---
+++ src/Dvchevskii.Result.Optional/ResultExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Dvchevskii.Optional;
+
+namespace Dvchevskii.Result.Optional
+{
+    public static class ResultExtensions
+    {
+        public static Option<T> Ok<T, E>(this IResult<T, E> self) =>
+            self.MapOrElse(Option.Some, _ => Option.None<T>());
+
+        public static Option<E> Err<T, E>(this IResult<T, E> self) =>
+            self.MapOrElse(_ => Option.None<E>(), Option.Some);
+
+        /// <summary>
+        /// Transposes a <see cref="IResult"/> on an <see cref="Option"/> to an <see cref="Option"/> of a <see cref="IResult"/>
+        /// </summary>
+        /// <param name="self"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="E"></typeparam>
+        /// <returns></returns>
+        public static Option<IResult<T, E>> Transpose<T, E>(this IResult<Option<T>, E> self) =>
+            self.Match(
+                opt =>
+                    opt.MapOrElse(v => Option.Some(Result.Ok<T, E>(v)), Option.None<IResult<T, E>>),
+                e => Option.Some(Result.Err<T, E>(e))
+            );
+    }
+}
---
+++ test/Dvchevskii.Result.Optional.Tests/ResultExtensionsTests.cs
@@ -0,0 +1,14 @@
+﻿using Dvchevskii.Optional;
+using FluentAssertions;
+
+namespace Dvchevskii.Result.Optional.Tests;
+
+[TestClass]
+public class ResultExtensionsTests
+{
+    [TestMethod]
+    public void Test_Ok()
+    {
+        Result.Ok(42).Ok().Should().Be(Option.Some(42));
+    }
+}
