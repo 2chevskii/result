1a8101779796e75d6698ffa69b002aa304fc5f9a
2CHEVSKII <prsroman3@gmail.com>
2024-01-27 02:11:47 +0300

    chore: formatting fix

--- test/Dvchevskii.Result.Tests/PatternMatchingTests.cs
+++ test/Dvchevskii.Result.Tests/PatternMatchingTests.cs
@@ -48,32 +48,40 @@ public class PatternMatchingTests
     [TestMethod]
     public void Test_Err()
     {
-        (Result.Err<int>(new Exception()) switch
-        {
-            IErr => true,
-            _ => false
-        }).Should().BeTrue();
+        (
+            Result.Err<int>(new Exception()) switch
+            {
+                IErr => true,
+                _ => false
+            }
+        ).Should().BeTrue();
 
-        (Result.Ok(42) switch
-        {
-            IErr => true,
-            _ => false
-        }).Should().BeFalse();
+        (
+            Result.Ok(42) switch
+            {
+                IErr => true,
+                _ => false
+            }
+        ).Should().BeFalse();
     }
 
     [TestMethod]
     public void Test_ErrGeneric()
     {
-        (Result.Err<int>(new Exception("__test__")) switch
-        {
-            IErr<Exception> e => e.Error.Message == "__test__",
-            _ => false
-        }).Should().BeTrue();
+        (
+            Result.Err<int>(new Exception("__test__")) switch
+            {
+                IErr<Exception> e => e.Error.Message == "__test__",
+                _ => false
+            }
+        ).Should().BeTrue();
 
-        (Result.Ok(42) switch
-        {
-            IErr<Exception> e => e.Error.Message == "__test__",
-            _ => false
-        }).Should().BeFalse();
+        (
+            Result.Ok(42) switch
+            {
+                IErr<Exception> e => e.Error.Message == "__test__",
+                _ => false
+            }
+        ).Should().BeFalse();
     }
 }
