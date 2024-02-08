deb6dd973405d002aec12f29d4f901900fd19ca8
2CHEVSKII <prsroman3@gmail.com>
2024-02-08 03:17:06 +0300

    feat: add MorphOk<R> and MorphErr<R> methods to Result<T,E>

---
+++ src/Dvchevskii.Result/Internal/Result.TypeConversion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Dvchevskii.Result.Exceptions;
+
+namespace Dvchevskii.Result
+{
+    public abstract partial class Result<T, E>
+    {
+        public virtual Result<T, R> MorphOk<R>() =>
+            MapOrElse(
+                Ok<T, R>,
+                _ =>
+                    throw new UnexpectedResultException(
+                        "Result state expected to be " + ResultState.Ok.ToString("G")
+                    )
+            );
+
+        public virtual Result<R, E> MorphErr<R>() =>
+            MapOrElse(
+                _ =>
+                    throw new UnexpectedResultException(
+                        "Result state expected to be " + ResultState.Err.ToString("G")
+                    ),
+                Err<R, E>
+            );
+    }
+}
---
+++ test/Dvchevskii.Result.Tests/TypeConversionTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+
+namespace Dvchevskii.Result.Tests;
+
+[TestClass]
+public class TypeConversionTests
+{
+    private Result<int, float> ConvertOk()
+    {
+        var result = Result.Ok(42);
+        return result.MorphOk<float>();
+    }
+
+    private Result<float, Exception> ConvertErr()
+    {
+        var result = Result.Err<int>(new Exception());
+        return result.MorphErr<float>();
+    }
+
+    [TestMethod]
+    public void Test_OkResultTypeConversion()
+    {
+        ConvertOk().State().Should().Be(ResultState.Ok);
+        ConvertOk().Unwrap().Should().Be(42);
+        ConvertOk().Should().BeAssignableTo<Result<int, float>>();
+    }
+
+    [TestMethod]
+    public void Test_ErrResultTypeConversion()
+    {
+        ConvertErr().State().Should().Be(ResultState.Err);
+        ConvertErr().UnwrapErr().Should().NotBeNull();
+        ConvertErr().Should().BeAssignableTo<Result<float, Exception>>();
+    }
+}
