f3f54e2a68016b5ed711bfdcd82acd17a364a920
2CHEVSKII <prsroman3@gmail.com>
2024-02-11 00:35:31 +0300

    feat: wip unit ex

---
+++ src/Dvchevskii.Result.Extensions.Unit/Result.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Dvchevskii.Result.Extensions.Unit
+{
+    public abstract class Result : Dvchevskii.Result.Result
+    {
+        public static Result<Dvchevskii.Unit.Unit, Exception> Ok()
+        {
+            return Dvchevskii.Result.Result.Ok(Dvchevskii.Unit.Unit.Default);
+        }
+
+        public static Result<Dvchevskii.Unit.Unit, E> Ok<E>()
+        {
+            return Dvchevskii.Result.Result.Ok<Dvchevskii.Unit.Unit, E>(
+                Dvchevskii.Unit.Unit.Default
+            );
+        }
+    }
+}
